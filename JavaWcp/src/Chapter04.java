3c5bfda4afe1183af7975d3f1312bca9e4cf06a4
岡隆一 <okaryuuichi@okaryuuichinoMacBook-Air.local>
2022-08-04 14:12:54 +0900

    2022-08-04

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,11 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String str = "岡　隆一";
+		final int LUCKY_NUMBER = 7;
+		System.out.println(str);
+		str = "好きこそものの上手なれ";
+		System.out.println(str);
+		System.out.println(LUCKY_NUMBER);
+	}
+}
