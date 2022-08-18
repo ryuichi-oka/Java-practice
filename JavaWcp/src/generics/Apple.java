a033777cc126b097a11a94f2306d39ae36f94b90
ryuichi-oka <98797052+ryuichi-oka@users.noreply.github.com>
2022-08-18 16:23:18 +0900

    応用4章復習

---
+++ JavaWcp/src/generics/Apple.java
@@ -0,0 +1,19 @@
+package generics;
+
+import java.math.BigDecimal;
+
+class Apple extends Fruit {
+	Apple(String name, BigDecimal price) {
+		super(name, price);
+	}
+	
+	@Override
+	protected String getName() {
+		return name;
+	}
+	
+	@Override
+	protected BigDecimal getPrice() {
+		return price;
+	}
+}
---
+++ JavaWcp/src/generics/Peach.java
@@ -0,0 +1,19 @@
+package generics;
+
+import java.math.BigDecimal;
+
+class Peach extends Fruit {
+	Peach(String name, BigDecimal price) {
+		super(name, price);
+	}
+	
+	@Override
+	protected String getName() {
+		return name;
+	}
+	
+	@Override
+	protected BigDecimal getPrice() {
+		return price;
+	}
+}
