30e82d2341f9d0a6b349ef29541a5d5059c77f0d
ryuichi-oka <98797052+ryuichi-oka@users.noreply.github.com>
2022-08-28 11:42:33 +0900

    null安全性

---
+++ Practice/src/part1/chapter2_5/Main.java
@@ -0,0 +1,24 @@
+package part1.chapter2_5;
+
+import java.util.Optional;
+
+// Optionalを利用してnull安全性に配慮する
+public class Main {
+	// 文字列sを文字列cで挟んで装飾するメソッド
+	// ・文字列sがnullまたは長さ0の場合は、nullを返す
+	// ・戻り値は「nullの可能性がある」ことを明示するために、単なるStringではなくOptional<String>とする
+	public static Optional<String> decorate(String s, char c) {
+		String r;
+		if (s == null || s.length() == 0) {
+			r = null;
+		} else {
+			r = c + s + c;
+		}
+		return Optional.ofNullable(r);
+	}
+	
+	public static void main(String[] args) {
+		Optional<String> s = decorate("", '*');
+		System.out.println(s.orElse("nullのため処理できません"));
+	}
+}
